c257a1eef932f1820c2e2615f45a87ae1db310a9
Firni11 <Firni11@users.noreply.github.com>
2016-11-03 20:51:50 +0100

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,6 @@
+print "Where do you live? ",
+village = raw_input ()
+print "What is your favorit country?",
+country = raw_input()
+tel_nr = raw_input ("Tell me your Tel. Nr.")
+print village, country, tel_nr
